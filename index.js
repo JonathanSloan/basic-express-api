f246fd87d07f651e82f0d6f43f277af60aaecabe
JonathanSloan <jonathanssloan@protonmail.com>
2020-08-11 11:15:41 -0400

    Added 404 & 500 handlers

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { request, response } = require("express");
 const app = express();
 const port = 3000;
 
@@ -15,8 +16,9 @@ app.get("/status", (request, response) => {
   response.status(200).json({ message: `ok`, status: 200 });
 });
 
-app.post("/signup", (request, response) => {
-  response.status(200).json({ message: `ok`, status: 200 });
+app.post("/signup", (request, response, next) => {
+  next(new Error(`Test Error`));
+  // response.status(200).json({ message: `ok`, status: 200 });
 });
 
 app.post("/login", (request, response) => {
@@ -37,6 +39,19 @@ app.post("/forgot-password", (request, response) => {
 
 app.post("/reset-password", (request, response) => {
   response.status(200).json({ message: `reset-password`, status: 200 });
+});
+
+// catch all other routes
+app.use((request, response) => {
+  response.status(404).json({ message: `404 - Not Found`, status: 404 });
+});
+
+// handle errors - should always be last route
+app.use((error, request, response, next) => {
+  console.log(error);
+  response
+    .status(error.status || 500)
+    .json({ error: error.message, status: 500 });
 });
 
 app.listen(port, () => {
