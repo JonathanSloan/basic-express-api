2655fa67e2c049769c38e1e61dd29d4d889263d6
JonathanSloan <jonathanssloan@protonmail.com>
2020-08-18 20:20:56 -0400

    Added: bodyParser for Express; Updated: index.js;
    Global
    - Added bodyParser from Express
    
    index.js
    - Added responses dependent on request's body for emails and token

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const { request, response } = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-// app.get("/", (request, response) => {
-// response.send(``);
-// response.status(200).json({ message: `ok`, status: 200 });
-// });
+// update express settings
+app.use(bodyParser.urlencoded({ extended: false })); // parse appliation/x-www-form-urlencoded data
+app.use(bodyParser.json()); // parse application/json
 
 app.get("/", (request, response) => {
   response.send(`Hello World!`);
@@ -17,28 +17,66 @@ app.get("/status", (request, response) => {
 });
 
 app.post("/signup", (request, response, next) => {
-  next(new Error(`Test Error`));
-  // response.status(200).json({ message: `ok`, status: 200 });
+  if (!request.body) {
+    response.status(400).json({ message: `Invalid Body`, status: 400 });
+  }
+
+  response.status(200).json({ message: `ok`, status: 200 });
 });
 
 app.post("/login", (request, response) => {
+  if (!request.body) {
+    response.status(400).json({ message: `Invalid Body`, status: 400 });
+  }
+
   response.status(200).json({ message: `login`, status: 200 });
 });
 
 app.post("/logout", (request, response) => {
+  if (!request.body) {
+    response.status(400).json({ message: `Invalid Body`, status: 400 });
+  }
+
   response.status(200).json({ message: `logout`, status: 200 });
 });
 
 app.post("/token", (request, response) => {
-  response.status(200).json({ message: `token`, status: 200 });
+  if (!request.body || !request.body.refreshToken) {
+    response.status(400).json({ message: `Invalid Body`, status: 400 });
+  } else {
+    const { refreshToken } = request.body;
+
+    response.status(200).json({
+      message: `refresh requested for token: ${refreshToken}`,
+      status: 200,
+    });
+  }
 });
 
 app.post("/forgot-password", (request, response) => {
-  response.status(200).json({ message: `forgot-password`, status: 200 });
+  if (!request.body || !request.body.email) {
+    response.status(400).json({ message: `Invalid Body`, status: 400 });
+  } else {
+    const { email } = request.body;
+
+    response.status(200).json({
+      message: `password requested for email: ${email}`,
+      status: 200,
+    });
+  }
 });
 
 app.post("/reset-password", (request, response) => {
-  response.status(200).json({ message: `reset-password`, status: 200 });
+  if (!request.body || !request.body) {
+    response.status(400).json({ message: `Invalid Body`, status: 400 });
+  } else {
+    const { email } = request.body;
+
+    response.status(200).json({
+      message: `password reset requested for email: ${email}`,
+      status: 200,
+    });
+  }
 });
 
 // catch all other routes
